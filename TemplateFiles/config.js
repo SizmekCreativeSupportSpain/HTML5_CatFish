3129e81ee743c2943d11f140f9e5e52dfeee2ddf
Javier Egido <javier.egido.alonso@gmail.com>
2019-10-30 12:58:52 +0100

    update config.js file

--- TemplateFiles/config.js
+++ TemplateFiles/config.js
@@ -1,39 +1,39 @@
 define({
-	format: "expandableBanner",
-	defaultBanner: "Main_Banner",
-	defaultPanel: "contracted",
-	polite: "instant",
-	banners: [
+	"format": "expandableBanner",
+	"defaultBanner": "Main_Banner",
+	"defaultPanel": "contracted",
+	"polite": "instant",
+	"banners": [
 		{
-			name: "Main_Banner",
-			asset: "index.html",
-			width: "1",
-			height: "1",
-			defaultImage: "images/1x1.jpg"
+			"name": "Main_Banner",
+			"asset": "index.html",
+			"width": "1",
+			"height": "1",
+			"defaultImage": "images/1x1.jpg"
 		}
 	],
-	panels: [
+	"panels": [
 		{
-			name: "contracted",
-			asset: "panels/contracted/index.html",
-			width: "320",
-			height: "50",
-			autoCollapse: "never",
-			delayedExpansion: "boolean",
-			positionType: "pageRelativePercentage",
-			x: "50",
-			y: "100"
+			"name": "contracted",
+			"asset": "panels/contracted/index.html",
+			"width": "320",
+			"height": "50",
+			"autoCollapse": "never",
+			"delayedExpansion": "false",
+			"positionType": "pageRelativePercentage",
+			"x": "50",
+			"y": "100"
 		},
 		{
-			name: "expanded",
-			asset: "panels/expanded/index.html",
-			width: "0",
-			height: "0",
-			autoCollapse: "never",
-			delayedExpansion: "boolean",
-			positionType: "pageRelativePercentage",
-			x: "0",
-			y: "0"
+			"name": "expanded",
+			"asset": "panels/expanded/index.html",
+			"width": "0",
+			"height": "0",
+			"autoCollapse": "never",
+			"delayedExpansion": "false",
+			"positionType": "pageRelativePercentage",
+			"x": "0",
+			"y": "0"
 		}
 	]
 });
